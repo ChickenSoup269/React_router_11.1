a58cdfbe9fcf606e82ee158063d4160ec329571d
MenterTho <kaosks123@gmail.com>
2024-11-12 23:27:05 +0700

    fix done

--- src/components/navigate.js
+++ src/components/navigate.js
@@ -1,15 +1,15 @@
 // components/Navigation.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 function Navigation() {
   return (
     <nav className="nav-container">
       <ul className="nav-list">
-        <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
-        <li className="nav-item"><Link to="/about" className="nav-link">About</Link></li>
-        <li className="nav-item"><Link to="/menu" className="nav-link">Menu</Link></li>
         
+        <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+        <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
+        <li className="nav-item"><a className="nav-link" href="/menu">Menu</a></li>
       </ul>
     </nav>
   );
